5ae02daf929872367edeb9ebdde2bf9842cde853
HayaMAM <hayamousa0088@gmail.com>
2019-04-19 07:11:57 +0300

    first commit

---
+++ MemeMe/ViewController.swift
@@ -0,0 +1,174 @@
+//
+//  ViewController.swift
+//  MemeMe
+//
+//  Created by Haya Mousa on 12/04/2019.
+//  Copyright © 2019 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIImagePickerControllerDelegate,
+UINavigationControllerDelegate, UITextFieldDelegate {
+
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var cameraButton: UIButton!
+    @IBOutlet weak var toolBarBottom: UIToolbar!    
+    @IBOutlet weak var ttop: UITextField!
+    @IBOutlet weak var tbottom: UITextField!
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    
+    override func viewWillAppear(_ animated: Bool){
+        super.viewWillAppear(animated)
+        
+        cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera)
+//        shareButton.isEnabled = false
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black ,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth: -4.0
+        ]
+
+        func configureTextfield(textfield: UITextField, defaultText: String) {
+            textfield.delegate = self
+            textfield.defaultTextAttributes = memeTextAttributes
+            textfield.textAlignment = .center
+            textfield.text = defaultText
+        }
+        configureTextfield(textfield: ttop, defaultText: "TOP")
+        configureTextfield(textfield: tbottom, defaultText: "BOTTOM")
+    }
+
+
+    @IBAction func pickAnImageFromAlbum(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true, completion: nil)
+    
+        }
+    
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]){
+        dismiss(animated: true, completion: nil)
+        if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+            imagePickerView.image = image
+
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController){
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromCamera(_ sender: Any) {
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func cancel(_ sender: Any) {
+        ttop.text = "TOP"
+        tbottom.text = "BOTTOM"
+    }
+    
+    
+    
+//    TEXT FIELD DELEGATE
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.text == "TOP" || textField.text == "BOTTOM" {
+            textField.text = ""
+        }
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func subscribeToKeyboardNotifications() {
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+        
+        view.frame.origin.y -= getKeyboardHeight(notification)
+    }
+    
+    @objc func keyboardWillHide(_ notification: Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    
+    func generateMemedImage() -> UIImage {
+
+        toolBarBottom.isHidden = true
+        navigationController?.setNavigationBarHidden(true, animated: true)
+
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+
+        toolBarBottom.isHidden = false
+        navigationController?.setNavigationBarHidden(false, animated: true)
+
+            return memedImage
+    }
+
+    struct Meme {
+        var topText: String
+        var bottomText: String
+        var originalImage: UIImage
+        var memedImage: UIImage
+    }
+    
+    func save() {
+        // Create the meme
+        _ = Meme(topText: ttop.text!, bottomText: tbottom.text!, originalImage: imagePickerView.image!, memedImage: generateMemedImage())
+    }
+    
+    @IBAction func share(_ sender: UIBarButtonItem) {
+        let memeImage: UIImage = generateMemedImage()
+        let controller = UIActivityViewController(activityItems: [memeImage], applicationActivities: nil)
+        controller.completionWithItemsHandler = {( type, complete, items, error ) in
+            if complete {
+                self.save()
+            }
+        }
+        self.present(controller, animated: true, completion: nil)
+    }
+    
+    
+}
+    
+
